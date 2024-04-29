0f4cefffcd6e215fd8b666a09015f32abf70b2dd
Tahsin0909 <tajisan789@gmail.com>
2024-04-30 01:31:22 +0600

    Added Employee Salary

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../Components/Navbar/Navbar';
 
 const MainLayout = () => {
     return (
-        <div className='flex gap-4 max-w-screen-xl mx-auto'>
+        <div className='flex '>
             <Navbar/>
             <Outlet/>
         </div>
