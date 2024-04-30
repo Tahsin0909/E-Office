26883e7ee804ff0c3109b078a2ddccdd65808dfa
Tahsin0909 <tajisan789@gmail.com>
2024-05-01 02:07:57 +0600

    Added heading in payroll and chnaged svg in btn

--- src/Components/Payroll_Items/PayrollItems.jsx
+++ src/Components/Payroll_Items/PayrollItems.jsx
@@ -1,9 +1,13 @@
 
 const PayrollItems = () => {
     return (
-        <div>
-            PayrollItems
+        <div className="md:p-4 py-2">
             
+            {/* heading  */}
+            <div>
+                <h1 className="text-2xl font-bold text-blue-700">Payroll Items</h1>
+                <p className="text-sm">E-Office / <a href="/payrollItem">payrollItem</a></p>
+            </div>
         </div>
     );
 };
